b6fc2ea464b314a6dda2d7b68a1d54190e122659
thanug <guruprasad828189@gmail.com>
2024-09-08 21:02:11 +0530

    added variables

--- variables.sh
+++ variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 PERSON1=guru
-PERSON2=bhav
+PERSON2=prasad
 
-echo "$PERSON1:: learning devops"
-ech0 "$PERSON2:: learning github"
+echo "hello $PERSON1, how are you"
+echo "hi, this is $PERSON2, i am doing good. Thanks afor asking"
+echo " what is going on $PERSON1"
+echo "i am learning devops $PERSON2"

5616ec471cc2e3e40a66f10e28d2c1538c542b90
thanug <guruprasad828189@gmail.com>
2024-09-08 21:17:29 +0530

    deleted files

--- variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PERSON1=guru
-PERSON2=prasad
-
-echo "$PERSON1:: Hi ${PERSON2}, How are you?"
-echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
-echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
-echo "${PERSON2}:: I started learning Shell Script $PERSON1"

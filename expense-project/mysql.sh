431ac0958010ffb7c19873c9e13dfdb1173b13a7
thanug <guruprasad828189@gmail.com>
2024-09-09 21:25:20 +0530

    commit

--- expense-project/mysql.sh
+++ expense-project/mysql.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-#checking user has root access or not
- USERID=$(id -u)
- INSTANCE_USER=$(whoami)
- #echo "user ID is: $USERID"
- 
-if [$USERID -ne 0]
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+if [ $USERID -ne 0 ]
 then
-   echo "Currently logged in as: $(whoami)"
-   
+    echo "Please run this script with root priveleges"
+    exit 1
 fi

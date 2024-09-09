4fd61e00081919e9c68c4f08b45aaa7ba46b06f0
thanug <guruprasad828189@gmail.com>
2024-09-09 20:59:08 +0530

    user

--- expense-project/mysql.sh
+++ expense-project/mysql.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #checking user has root access or not
-USERID= $(id -u)
+ USERID=$(id -u)
  #echo "user ID is: $USERID"
  
 
- if [$USERID -ne 0]
- then
-  echo "User does not have root priviliges"
-  
+if [$USERID -ne 0]
+   then
+     echo "User does not have root priviliges" 
 else
    echo "Currently logged in as: $(whoami)"
    exit 1

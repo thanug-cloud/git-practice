43b7906f78c9b1d48256ae01e1e7e32a6c6cc885
thanug <guruprasad828189@gmail.com>
2024-09-09 20:14:50 +0530

    mod

--- conditions.sh
+++ conditions.sh
@@ -3,7 +3,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 50 ]
 then
-    echo "given number is : $NUMBER is greater than 50"
+    echo "given number is: $NUMBER is greater than 50"
 else
-    echo "given number is : $NUMBER is less than 50"
+    echo "given number is: $NUMBER is less than 50"
 fi

b80041e0d979d4a0e3579d55d711aec1c9c98aa3
thanug <guruprasad828189@gmail.com>
2024-09-09 21:55:02 +0530

    QA

---
+++ expense-project/QA_update_mysql.sh
@@ -0,0 +1,9 @@
+USERID=$(id -u)
+INSTANCE_USER=$(whoami)
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root privileges"
+    exit 1
+else
+    echo "Currently logged in as: $INSTANCE_USER"
+    exit 0
+fi

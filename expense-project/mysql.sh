b98215f96efe4539b6fbb41b0a863e567a56829f
thanug <guruprasad828189@gmail.com>
2024-09-09 20:54:42 +0530

    user access

--- expense-project/mysql.sh
+++ expense-project/mysql.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #checking user has root access or not
 USERID= $(id -u)
-INSTANCE_USER= $(whoami)
- echo "user ID is: $USERID"
+ #echo "user ID is: $USERID"
  
 
  if [$USERID -ne 0]
  then
-  echo "INSTANCE_USER: $whoami does not have priviliges to run this script"
+  echo "User does not have root priviliges"
+  
 else
-   echo "user has root privileges...proceeding further.
+   echo "Currently logged in as: $(whoami)"
    exit 1
 fi
